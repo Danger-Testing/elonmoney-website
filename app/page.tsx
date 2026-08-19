c77ef91e4595383e527ae4aa897812d770ddec5e
Marc Mueller <91703789+marcgmbh@users.noreply.github.com>
2026-08-19 10:30:25 -0400

    Build Figma landing page

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="landing" data-node-id="43:15">
+      <h1 className="landing__title" data-node-id="43:16">
+        Elon Money
+      </h1>
+
+      <button className="landing__button" type="button" data-node-id="43:19">
+        <span data-node-id="43:20">Net worth</span>
+      </button>
+    </main>
+  );
+}
